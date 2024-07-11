9b2c864d235b25f908dbbdeca4471a3b322db49f
Silvanos Eric <ericoderito@gmail.com>
2024-07-11 12:52:22 +0300

    feat: add array manipulation functions for cats and update test results
    Implement new functions in index.js to append, prepend, remove last, and remove first cat from the
    array both destructively and non-destructively. Add corresponding test results in .results.json to
    reflect these new capabilities.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const newCatsArray = [...cats];
+  newCatsArray.push(name);
+  return newCatsArray;
+}
+
+function prependCat(name) {
+  const newCatsArray = [...cats];
+  newCatsArray.unshift(name);
+  return newCatsArray;
+}
+
+function removeLastCat() {
+  const newCatsArray = [...cats];
+  newCatsArray.pop();
+  return newCatsArray;
+}
+
+function removeFirstCat() {
+  const newCatsArray = [...cats];
+  newCatsArray.shift();
+  return newCatsArray;
+}
